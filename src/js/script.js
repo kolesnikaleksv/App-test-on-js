7f3565be4af7137470002021eb54f9f54904ce1b
kolesnikaleks <kolesnikaleksv@gmail.com>
2020-05-30 13:18:23 +0300

    solution on the second task

--- src/js/script.js
+++ src/js/script.js
@@ -17,28 +17,59 @@
 
 // console.log(`привет ${answers[1]} ${answers[0]} ${answers[2]}`);
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+ const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 
- let a = prompt("Oдин из последних просмотренных фильмов?", ''),
-     b = prompt("Oдин из последних просмотренных фильмов?", ''),
-     c = prompt("На сколько оцените его?", ""),
-     d = prompt("На сколько оцените его?", "");
- 
+//  let a = prompt("Oдин из последних просмотренных фильмов?", ''),
+//      b = prompt("Oдин из последних просмотренных фильмов?", ''),
+    //  c = prompt("На сколько оцените его?", "");
+//      d = prompt("На сколько оцените его?", "");
+
+
+
 const personalMovieDB = {
     count: `${numberOfFilms}`,
-    movies: {
-        // lastFilma: `${lastFilma}`,
-        // lastFilm: `${lastFilm}`,
-        // valueIta: `${valueIta}`,
-        // valueIt: `${valueIt}`
-        
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-personalMovieDB.movies[a] = c;
-personalMovieDB.movies[b] = d;
+    let i = 0;
+
+
+
+    do {
+        let a = prompt("Oдин из последних просмотренных фильмов?", ''),
+        c = prompt("На сколько оцените его?", "");
+        i++;
+        personalMovieDB.movies[a] = c;
+    }
+    while( i < 2);
+
+
+    // for (let i=0; i<2; i++) {
+    //     let a = prompt("Oдин из последних просмотренных фильмов?", ''),
+    //         c = prompt("На сколько оцените его?", "");
+            
+            
+    //     (a.length && c.length && a.length < 5 && c.length < 5) ? personalMovieDB.movies[a] = c : i-- ;
+            
+    // }
+ 
+
+    
+    // if (numberOfFilms <= 10) {
+    //     alert('Просмотренно мало фильмов');
+    // } else if (numberOfFilms.length > 10 && numberOfFilms.length < 30) {
+    //     alert("Вы классический зритель");
+    // } else if (numberOfFilms.length >= 30) {
+    //     alert("Вы киноман");
+    // } else {
+    //     alert("Произошла ошибкa");
+    // }
+
+// personalMovieDB.movies[a] = c;
+// personalMovieDB.movies[b] = d;
+
 console.log(personalMovieDB);
